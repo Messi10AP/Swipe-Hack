9e678d70bf2d82a31821f5a25c8ae228f9dd89f8
Poojit <phalpha1729@gmail.com>
2018-01-21 07:40:47 -0500

    Finalv1

--- penn.py
+++ penn.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageTk
 strings = 'qwertyuiopasdfghjklzxcvbnm'
 from matplotlib import pyplot as plt
 
-
+out=False
 VERBOSE = True
 vowels = set('aeiouy')
 alphabet = set('abcdefghijklmnopqrstuvwxyz')
@@ -187,7 +187,7 @@ def spellchecker(word):
 	word_model = train_from_files(texts, word_model)
 	while True:
 		word1 = word
-		word = recognizeString(5,word1)
+		word = recognizeString(4,word1)
 		possibilities = suggestions(word, real_words, short_circuit=False)
 		short_circuit_result = suggestions(word, real_words, short_circuit=True)
 		if VERBOSE:
@@ -258,212 +258,243 @@ def letter(currposition):
 
 coordinates = []
 letters = []
-#ser = Serial('/dev/ttyACM1', 9600)
-cam = cv2.VideoCapture(1)
+ser = Serial('/dev/ttyACM0', 9600)
+cam = cv2.VideoCapture(0)
 #cam.set(cv2.CAP_PROP_AUTOFOCUS, 1)
 cam.set(3,1280)
 cam.set(4,720)
 #cam.set(
 #time.sleep(2)
 #cam.set(15,8.0)
-#try:11,11
-#	s = ser.readline()
-#except: 
-#	continue
-#time.sleep(5)
-
-kernel = np.ones((11,11),np.uint8)
-
-while True:	
-	ret, im = cam.read()
-	im = cv2.flip( im, 0 )
-	im = cv2.flip( im, 1 )
-	cv2.rectangle(im, (40,220), (1240,580), (255,0,0), 3)
-	cv2.imshow("img", im)
-	k=cv2.waitKey(33)
-	if k == 27:
-		break
-
-#lab= cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
-#l, a, b = cv2.split(lab)
-#clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-#cl = clahe.apply(l)
-#limg = cv2.merge((cl,a,b))
-#im = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-#t = turtle.Turtle()
-
-#screen.addshape(image)
-#turtle.shape(image)
-#a = np.double(im)
-#b = a + 15
-#im = np.uint8(b)
-#im=cv2.imread('board6.jpg')
-#im = cv2.resize(im, (1920, 1080))
-im = adjust_gamma(im, 0.5)
-im = im[220:580, 40:1240]
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-temp =  cv2.erode(imgray,kernel,iterations = 2)
-#temp = cv2.GaussianBlur(temp,(5,5),0)
-#temp = cv2.cvtColor(temp,cv2.COLOR_GRAY2RGB)
-imgray = (255-imgray)
-
-blur = cv2.GaussianBlur(imgray,(5,5),0)
-#blur = (255-blur)
-cv2.imshow("img", blur)
-#cv2.waitKey(0)
-#ret,thresh = cv2.threshold(blur,150,255,cv2.THRESH_BINARY)
-ret3,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-thresh = cv2.dilate(thresh,kernel,iterations = 1)
-thresh =  cv2.erode(thresh,kernel,iterations = 1)
-#temp=thresh
-cv2.imshow("img", thresh)
-cv2.waitKey(0)
-threshnew = (255-thresh)
-#imnext = cv2.dilate(thresh,kernel,iterations = 1)
-#threshnew = cv2.erode(threshnew,kernel,iterations = 1)
-cv2.imshow("img", threshnew)
-#cv2.waitKey(0)
-__, contours, __ = cv2.findContours(threshnew,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-print len(contours)	
-count = 0
-check=0
-actualcontours=[]
-y_coords = []
-x_coords = []
-
-for cnt in contours:
-	if cv2.contourArea(cnt) > 10 and cv2.contourArea(cnt) < 10000:
-		actualcontours.append(cnt)
-		
-		#print cv2.contourArea(cnt)
-		count+=1
-		#print count
-		x,y,w,h = cv2.boundingRect(cnt)
-		y_coords.append(y)
-		x_coords.append(x)
-		cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
-		#if y>50 and y+h < 660 and x>50 and x+w < 1230:
-			#roi = im[y-50:y+h+50, x-50:x+w+50]
-			#roi = (255-roi)
-			#roi= cv2.resize(roi, (426, 240))
-			#cv2.imshow("roi",roi)
-			#cv2.waitKey(0)
-			#cv2.imwrite("thresh.png",roi)
-			#text = pytesseract.image_to_string(Image.open('thresh.png'))
-			#print(text)
-	check+=1
-cv2.imshow('im', im)
-cv2.waitKey(0)
-#ret = cropInitialImage(temp, actualcontours, y_coords)
-#print ret[1]
-#print ret[0]
-#x,y,w,h = cv2.boundingRect(actualcontours[ret[1]])
-#cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,255),2)
-#xx,yy,ww,hh = cv2.boundingRect(actualcontours[ret[3]])
-#cv2.rectangle(im,(xx,yy),(xx+ww,yy+hh),(0,255,255),2)
-q,a,z,w = cropInitialImage(contours, x_coords)	
-#print q, a, z, w
-#print "AS"
-qx,qy,qw,qh = cv2.boundingRect(contours[q])
-#print qx,qy,qw,qh, "Q"
-#cv2.circle(im, (qx,qy), 45, (255,255,0))
-cv2.imshow("img", im)
-#cv2.waitKey(0)
-ax,ay,aw,ah = cv2.boundingRect(contours[a])
-zx,zy,zw,zh = cv2.boundingRect(contours[z])
-wx,wy,ww,wh = cv2.boundingRect(contours[w])
-DISTANCE = 128#wx-qx+16
-#print DISTANCE
-for i in range(10):
-	center = (qx+qw/2+DISTANCE*i, qy+qh/2)
-	coordinates.append(center)
-	letters.append(strings[i])
-for i in range(9):
-	center = (ax+aw/2+DISTANCE*i, ay+ah/2)
-	coordinates.append(center)
-	letters.append(strings[10+i])
-for i in range(7):
-	center = (zx+zw/2+DISTANCE*i, zy+zh/2)
-	coordinates.append(center)
-	letters.append(strings[19+i])
-#print coordinates
-for i in coordinates:
-	cv2.circle(im, i, 64, (255,255,0))
-	#print letters[coordinates.index(i)]
-	cv2.imshow("img", im)
-	cv2.waitKey(1)
-	#print letters
-cam.release()
-#cv2.imshow("img", im)
-#cv2.waitKey(0)
-cam2= cv2.VideoCapture(1)
-cam2.set(3,1280)
-cam2.set(4,720)
-#time.sleep(2)
-done = 0
-word=''
-start = False
-f = open("oldwords.txt", 'w+')
-g = open("newwords.txt", 'w+')
-prev_word = ""
+print "Waiting for Keyboard"
 while True:
-	ret, img = cam2.read()
-	img = cv2.flip( img, 0 )
-	img = cv2.flip( img, 1 )
-	img = img[220:580, 40:1240]
-	imgg = cv2.medianBlur(img,5)
-	hsv = cv2.cvtColor(imgg, cv2.COLOR_BGR2HSV)
-	mask = cv2.inRange(hsv, np.array([0,0,200]), np.array([180,20,255])) #+ cv2.inRange(hsv, np.array([170,100,100]), np.array([180,255,255]))
-	output_img = img.copy()
-	output_img[np.where(mask==0)] = 0
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
-	cv2.putText(img, prev_word, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,0,255),2,cv2.LINE_AA)
-	if len(cnts) > 0:
-		
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
- 
-		# only proceed if the radius meets a minimum size
-		#print radius
-		if radius > 50:
-			done=0
-			start = True
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-			cv2.circle(img, center, 5, (0, 0, 255), -1)
-			#print (x,y)
-			r = letter((x,y))
- 			cv2.putText(img,str(r),(int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,0,255),2,cv2.LINE_AA)
+	try:
+		s = ser.readline()
+		if not int(s):
+			continue
+		print "Keyboard Recognized"
+		kernel = np.ones((11,11),np.uint8)
+
+		while True:	
+			ret, im = cam.read()
+			im = cv2.flip( im, 0 )
+			im = cv2.flip( im, 1 )
+			cv2.rectangle(im, (40,220), (1240,580), (255,0,0), 3)
+			cv2.imshow("img", im)
+			k=cv2.waitKey(33)
+			if k == 27:
+				break
+
+		#lab= cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+		#l, a, b = cv2.split(lab)
+		#clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+		#cl = clahe.apply(l)
+		#limg = cv2.merge((cl,a,b))
+		#im = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+		#t = turtle.Turtle()
+
+		#screen.addshape(image)
+		#turtle.shape(image)
+		#a = np.double(im)
+		#b = a + 15
+		#im = np.uint8(b)
+		#im=cv2.imread('board6.jpg')
+		#im = cv2.resize(im, (1920, 1080))
+		im = adjust_gamma(im, 0.5)
+		im = im[220:580, 40:1240]
+		imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+		temp =  cv2.erode(imgray,kernel,iterations = 2)
+		#temp = cv2.GaussianBlur(temp,(5,5),0)
+		#temp = cv2.cvtColor(temp,cv2.COLOR_GRAY2RGB)
+		imgray = (255-imgray)
+
+		blur = cv2.GaussianBlur(imgray,(5,5),0)
+		#blur = (255-blur)
+		#cv2.imshow("img", blur)
 		#cv2.waitKey(0)
-			word+=r
-		else:
-			done+=1
-	cv2.imshow("img", img)
-	if done>=10 and start:
-		print word
-		f.write(word + "\n")
-		rest = spellchecker(word)
-		print "Word Detected: " + str(rest)
-		g.write(rest + "\n")
-		prev_word = rest
-		word = ''
+		#ret,thresh = cv2.threshold(blur,150,255,cv2.THRESH_BINARY)
+		ret3,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		thresh = cv2.dilate(thresh,kernel,iterations = 1)
+		thresh =  cv2.erode(thresh,kernel,iterations = 1)
+		#temp=thresh
+		#cv2.imshow("img", thresh)
+		#cv2.waitKey(0)
+		threshnew = (255-thresh)
+		#imnext = cv2.dilate(thresh,kernel,iterations = 1)
+		#threshnew = cv2.erode(threshnew,kernel,iterations = 1)
+		#cv2.imshow("img", threshnew)
+		#cv2.waitKey(0)
+		__, contours, __ = cv2.findContours(threshnew,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		print len(contours)	
+		count = 0
+		check=0
+		actualcontours=[]
+		y_coords = []
+		x_coords = []
+
+		for cnt in contours:
+			if cv2.contourArea(cnt) > 10 and cv2.contourArea(cnt) < 10000:
+				actualcontours.append(cnt)
+		
+				#print cv2.contourArea(cnt)
+				count+=1
+				#print count
+				x,y,w,h = cv2.boundingRect(cnt)
+				y_coords.append(y)
+				x_coords.append(x)
+				cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+				#if y>50 and y+h < 660 and x>50 and x+w < 1230:
+					#roi = im[y-50:y+h+50, x-50:x+w+50]
+					#roi = (255-roi)
+					#roi= cv2.resize(roi, (426, 240))
+					#cv2.imshow("roi",roi)
+					#cv2.waitKey(0)
+					#cv2.imwrite("thresh.png",roi)
+					#text = pytesseract.image_to_string(Image.open('thresh.png'))
+					#print(text)
+			check+=1
+		#cv2.imshow('im', im)
+		#cv2.waitKey(0)
+		#ret = cropInitialImage(temp, actualcontours, y_coords)
+		#print ret[1]
+		#print ret[0]
+		#x,y,w,h = cv2.boundingRect(actualcontours[ret[1]])
+		#cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,255),2)
+		#xx,yy,ww,hh = cv2.boundingRect(actualcontours[ret[3]])
+		#cv2.rectangle(im,(xx,yy),(xx+ww,yy+hh),(0,255,255),2)
+		q,a,z,w = cropInitialImage(contours, x_coords)	
+		#print q, a, z, w
+		#print "AS"
+		qx,qy,qw,qh = cv2.boundingRect(contours[q])
+		#print qx,qy,qw,qh, "Q"
+		#cv2.circle(im, (qx,qy), 45, (255,255,0))
+		#cv2.imshow("img", im)
+		#cv2.waitKey(0)
+		ax,ay,aw,ah = cv2.boundingRect(contours[a])
+		zx,zy,zw,zh = cv2.boundingRect(contours[z])
+		wx,wy,ww,wh = cv2.boundingRect(contours[w])
+		DISTANCE = 128#wx-qx+16
+		#print DISTANCE
+		for i in range(10):
+			center = (qx+qw/2+DISTANCE*i, qy+qh/2)
+			coordinates.append(center)
+			letters.append(strings[i])
+		for i in range(9):
+			center = (ax+aw/2+DISTANCE*i, ay+ah/2)
+			coordinates.append(center)
+			letters.append(strings[10+i])
+		for i in range(7):
+			center = (zx+zw/2+DISTANCE*i, zy+zh/2)
+			coordinates.append(center)
+			letters.append(strings[19+i])
+		#print coordinates
+		for i in coordinates:
+			cv2.circle(im, i, 64, (255,255,0))
+			#print letters[coordinates.index(i)]
+			cv2.imshow("img", im)
+			cv2.waitKey(1)
+			#print letters
+		cv2.destroyAllWindows()
+		cam.release()
+		#cv2.imshow("img", im)
+		#cv2.waitKey(0)
+		cam2= cv2.VideoCapture(0)
+		cam2.set(3,1280)
+		cam2.set(4,720)
+		#time.sleep(2)
 		done = 0
-		start=False
-		word = ""
-	cv2.imwrite("keyboard.png", img)
-	k=cv2.waitKey(33)
-	if k == 32:
-		f.close()
-		g.close()
-		break
-	else:
+		word=''
+		start = False
+		f = open("oldwords.txt", 'w+')
+		g = open("newwords.txt", 'w+')
+		last_loc = []
+		prev_word = ""
+		badcount=0
+		while True:
+			#s = ser.readline()
+			#if not int(s):
+				#cv2.putText(final,"Keyboard removed.",(500,500), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,0,0),2,cv2.LINE_AA)
+				#cv2.imshow('Glyde', final)
+				#badcount+=1
+				#continue
+		#	if badcount>10:
+		#		cv2.waitKey(3000)
+		#		cam2.release()
+		#		cv2.destroyAllWindows()
+		#		break
+			ret, img = cam2.read()
+			img = cv2.flip( img, 0 )
+			img = cv2.flip( img, 1 )	
+			img = img[220:720, 40:1240]
+			roi = img[0:220, 0:1200]
+			mask = np.ones(img.shape[:2], np.uint8)
+			mask[360:500,0:1200] = 0
+			img = cv2.bitwise_and(img,img,mask = mask)
+			imgg = cv2.medianBlur(img,5)
+			hsv = cv2.cvtColor(imgg, cv2.COLOR_BGR2HSV)
+			mask = cv2.inRange(hsv, np.array([0,0,200]), np.array([180,20,255])) #+ cv2.inRange(hsv, np.array([170,100,100]), np.array([180,255,255]))
+			output_img = img.copy()
+			output_img[np.where(mask==0)] = 0
+			mask = cv2.erode(mask, None, iterations=2)
+			mask = cv2.dilate(mask, None, iterations=2)
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+			center = None
+			cv2.putText(img, prev_word, (50, 420), cv2.FONT_ITALIC, 1,(255,255,255),1,cv2.LINE_AA)
+			if len(cnts) > 0:
+		
+				c = max(cnts, key=cv2.contourArea)
+				((x, y), radius) = cv2.minEnclosingCircle(c)
+				M = cv2.moments(c)
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		 
+				# only proceed if the radius meets a minimum size
+				#print radius
+				if radius > 50:
+					done=0
+					start = True
+					# draw the circle and centroid on the frame,
+					# then update the list of tracked points
+					last_loc.append((int(x), int(y)))	
+					cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+					cv2.circle(img, center, 5, (0, 0, 255), -1)
+					#print (x,y)
+					r = letter((x,y))
+		 			cv2.putText(img,str(r),(int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,0,0),2,cv2.LINE_AA)
+				#cv2.waitKey(0)
+					word+=r
+				else:
+					done+=1
+			for i in range(len(last_loc)-1):
+				cv2.line(img, last_loc[i], last_loc[i+1], (255,255,0),4)
+			height, width, channels = img.shape
+			final = cv2.resize(img, (0,0), fx=(float(1920)/(1.2*width)), fy=(float(1080)/(1.2*height)))
+			
+			cv2.imshow("Glyde", final)
+			if done>=10 and start:
+				print word
+				f.write(word + "\n")
+				rest = spellchecker(word)
+				print "Word Detected: " + str(rest)
+				g.write(rest + "\n")
+				prev_word += str(rest) + " "
+				word = ''
+				done = 0
+				start=False
+				word = ""
+				last_loc = []
+			cv2.imwrite("keyboard.png", img)
+			k=cv2.waitKey(33)
+			if k == 32:
+				f.close()
+				g.close()
+				out = True
+				cap2.release()
+				cv2.destroyAllWindows()
+				break
+			else:
+				continue
+	except: 
 		continue
 
 	
